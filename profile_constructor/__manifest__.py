17b930118309140f8818533127483374a1f9b4a9
Antony Castillo <89779489+Antonybriaanc@users.noreply.github.com>
2025-05-07 23:11:59 -0400

    Update __manifest__.py

--- profile_constructor/__manifest__.py
+++ profile_constructor/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Gestion de accesos de usuarios',
+    'name': 'access profile manager',
     'version': '1.0',
     'summary': 'Automatización avanzada de perfiles de usuario en Odoo',
     'description': (
