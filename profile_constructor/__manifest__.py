bcea48d55d144587ef6e94b3612b521231485a70
Antony Castillo <89779489+Antonybriaanc@users.noreply.github.com>
2025-05-07 23:14:45 -0400

    Update __manifest__.py

--- profile_constructor/__manifest__.py
+++ profile_constructor/__manifest__.py
@@ -14,16 +14,13 @@
         '- Ahorra tiempo al evitar la configuración manual de permisos para cada usuario.\n'
         '- Garantiza una correcta asignación de permisos en función de los roles empresariales, evitando errores manuales.\n'
         '- Gestiona usuarios de manera eficiente incluso en grandes equipos.\n\n'
-        'Instalación:\n'
-        '1. Clona este módulo en tu directorio de addons de Odoo.\n'
-        '2. Reinicia el servidor de Odoo.\n'
-        '3. Actualiza la lista de aplicaciones desde el backend y busca Profile Constructor para instalarlo.\n\n'
         'Guía de Uso:\n'
         '1. Accede al menú Ajustes > Perfiles de Usuario para crear nuevos perfiles.\n'
         '2. Asigna perfiles a usuarios desde Ajustes > Usuarios y Compañías > Usuarios.\n'
         '3. Modifica perfiles para ajustar permisos y actualiza automáticamente los perfiles asignados.\n\n'
         'Soporte y Contacto: '
-        'Desarrollado por Cosmo Solutions. Para soporte técnico, contacta a: cosmosolutions1@outlook.com.'
+        'Desarrollado por Cosmo Solutions. 
+            Para soporte técnico, contacta a: cosmosolutions1@outlook.com.'
     ),
     'author': 'Cosmo solutons',
     'maintainer': 'Cosmo Solutions',
